357b7c9fd8601ae1ecf56f4aec5c85b163af0dfe
Nooh-Shoaib <147729418+Nooh-Shoaib@users.noreply.github.com>
2023-12-04 17:59:51 +0500

    initial commit

--- src/pages/LegalPages.js
+++ src/pages/LegalPages.js
@@ -1,7 +1,8 @@
+// Import necessary modules and components
 import React, { useState, useEffect } from 'react';
-import Layout from '../layout';
 import { Link, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import Layout from '../layout';
 import LoadingComponent from './loading';
 import Url from './utils/Url';
 
@@ -24,13 +25,11 @@ const LegalPages = () => {
       const pageData = jsonPagedata.find((page) => page.slug === slug) || {};
       setPagedata(pageData);
       setLoading(false);
-      return pageData;
     } catch (error) {
       console.error(`Error fetching pagedata for ${slug}:`, error.message);
       setPagedata({});
       setLoading(false);
       setError(error);
-      return null;
     }
   };
 
@@ -40,14 +39,10 @@ const LegalPages = () => {
 
   return (
     <>
-      {/* <Helmet>
-        <head>
-          <title>{pagedata?.pagetitle || 'Page Title'}</title>
-          <meta property="og:description" content="Custom Packaging Boxes" />
-          <meta property="og:type" content="website" />
-
-        </head>
-      </Helmet> */}
+      <Helmet>
+        <title>{pagedata?.pagetitle || 'Page Title'}</title>
+        {/* Add other meta tags as needed */}
+      </Helmet>
 
       <Layout>
         {loading && <LoadingComponent />}
@@ -55,24 +50,25 @@ const LegalPages = () => {
         {!loading && !error && !pagedata && <div>Error: Unable to load page data</div>}
         {!loading && !error && pagedata && (
           <>
-            <h1 className="bg-slate-200 h-12 flex items-center">
-              <Link to="/" className="mx-7 font-bold hover:text-blue-600">
+            {pagedata.image && <img src={pagedata.image} alt='' className='w-full' />}
+
+            <h1 className="bg-slate-200 h-16 flex items-center">
+              <Link to="/" className="mx-4 font-bold hover:text-blue-600">
                 Home
               </Link>
-              <span className="text-xs">&raquo;&nbsp;&nbsp;</span>
-
+              <span className="text-xs">&raquo;&nbsp;&nbsp;&nbsp;</span>
               <span className="text-amber-500 font-bold">
                 <em>{pagedata?.pagetitle}</em>
               </span>
             </h1>
 
-            <div className='pl-48 py-12'>
+            <div className='px-48 py-12'>
               <h1 className="my-4 text-4xl font-semibold">{pagedata?.pagetitle}</h1>
               {pagedata?.description && pagedata?.description.map((data, index) => (
                 <div key={index} className="mb-6">
-                  <p>{data.firsttext}</p>
+                  <p className='text-base text-justify'>{data.firsttext}</p>
                   <h2 className='text-3xl font-semibold'>{data.heading}</h2>
-                  <p className='text-base'>{data.text}</p>
+                  <p className='text-base text-justify'>{data.text}</p>
                   {data.phone && (
                     <div className="mt-2">
                       <h2 className='text-base font-bold'>Phone: </h2>
@@ -100,4 +96,3 @@ const LegalPages = () => {
 };
 
 export default LegalPages;
-
